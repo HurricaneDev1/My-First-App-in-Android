ba67e844acb908e5485b2ac3f8c82e949b6dd266
HurricaneDev1 <hudsoncpoetree@gmail.com>
2022-08-29 12:40:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,29 @@
+package com.example.myfirstapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView2);
+        if(message.equals("Fork") | message.equals("fork")){
+            textView.setText("You truly have taste");
+        }else{
+            textView.setText("You said " + message + ". I'm not quite sure what to make of you");
+        }
+    }
+}
